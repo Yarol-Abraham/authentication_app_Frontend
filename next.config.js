bf6b9b70bf74290902c0202336e86947ebe77140
Abraham <59816968+Yarol-Abraham@users.noreply.github.com>
2021-06-29 23:15:46 -0600

    settings next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     env: {
-        REACT_APP_PROD_URL: 'https://authenticationya.herokuapp.com/',
+        REACT_APP_PROD_URL: 'https://authenticationya.herokuapp.com/api/v1/',
         REACT_APP_PROD_PHOTO: 'https://authenticationya.herokuapp.com/images/users/',
         REACT_APP_DEV_URL: 'http://localhost:5000/api/v1/',
         REACT_APP_DEV_PHOTO: 'http://localhost:5000/assets/images/users/',
